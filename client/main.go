6dec1f65acebe3f0587cca26e1f1df05c5cc21de
Max Lv <max.c.lv@gmail.com>
2016-08-10 12:59:55 +0800

    Add android callbacks

--- client/main.go
+++ client/main.go
@@ -1,5 +1,55 @@
 package main
 
+/*
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/uio.h>
+
+#define ANCIL_FD_BUFFER(n) \
+    struct { \
+	struct cmsghdr h; \
+	int fd[n]; \
+    }
+
+int
+ancil_send_fds_with_buffer(int sock, const int *fds, unsigned n_fds, void *buffer)
+{
+    struct msghdr msghdr;
+    char nothing = '!';
+    struct iovec nothing_ptr;
+    struct cmsghdr *cmsg;
+    int i;
+
+    nothing_ptr.iov_base = &nothing;
+    nothing_ptr.iov_len = 1;
+    msghdr.msg_name = NULL;
+    msghdr.msg_namelen = 0;
+    msghdr.msg_iov = &nothing_ptr;
+    msghdr.msg_iovlen = 1;
+    msghdr.msg_flags = 0;
+    msghdr.msg_control = buffer;
+    msghdr.msg_controllen = sizeof(struct cmsghdr) + sizeof(int) * n_fds;
+    cmsg = CMSG_FIRSTHDR(&msghdr);
+    cmsg->cmsg_len = msghdr.msg_controllen;
+    cmsg->cmsg_level = SOL_SOCKET;
+    cmsg->cmsg_type = SCM_RIGHTS;
+    for(i = 0; i < n_fds; i++)
+	((int *)CMSG_DATA(cmsg))[i] = fds[i];
+    return(sendmsg(sock, &msghdr, 0) >= 0 ? 0 : -1);
+}
+
+int
+ancil_send_fd(int sock, int fd)
+{
+    ANCIL_FD_BUFFER(1) buffer;
+
+    return(ancil_send_fds_with_buffer(sock, &fd, 1, &buffer));
+}
+
+*/
+import "C"
+
 import (
 	"crypto/sha1"
 	"io"
@@ -7,6 +57,7 @@ import (
 	"math/rand"
 	"net"
 	"os"
+    "syscall"
 	"time"
 
 	"golang.org/x/crypto/pbkdf2"
@@ -117,6 +168,11 @@ func main() {
 			Name:  "mode",
 			Value: "fast",
 			Usage: "mode for communication: fast3, fast2, fast, normal",
+		},
+		cli.StringFlag{
+			Name:  "path",
+			Value: "/data/data/com.github.shadowsocks/protect_path",
+			Usage: "path to shadowsocks data dir",
 		},
 		cli.IntFlag{
 			Name:  "conn",
@@ -247,6 +303,35 @@ func main() {
 		log.Println("sockbuf:", sockbuf)
 		log.Println("keepalive:", keepalive)
 		log.Println("conn:", conn)
+
+		path := c.String("path")
+		net.Callback = func(fd int) {
+			socket, err := syscall.Socket(syscall.AF_UNIX, syscall.SOCK_STREAM, 0)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			defer syscall.Close(socket)
+
+			err = syscall.Connect(socket, &syscall.SockaddrUnix{Name: path})
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			C.ancil_send_fd(C.int(socket), C.int(fd))
+
+			dummy := []byte{1}
+			n, err := syscall.Read(socket, dummy)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			if n != 1 {
+				log.Println("Failed to protect fd: ", fd)
+				return
+			}
+		}
 
 		createConn := func() *yamux.Session {
 			kcpconn, err := kcp.DialWithOptions(remoteaddr, block, datashard, parityshard)
