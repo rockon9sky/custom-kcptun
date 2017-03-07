8e16082b92b4d9fba534a850347fd5638c802866
Max Lv <max.c.lv@gmail.com>
2017-03-07 11:02:03 +0800

    Remove deprecated SetKeepAlive

--- server/main.go
+++ server/main.go
@@ -461,7 +461,6 @@ func main() {
 				conn.SetMtu(config.MTU)
 				conn.SetWindowSize(config.SndWnd, config.RcvWnd)
 				conn.SetACKNoDelay(config.AckNodelay)
-				conn.SetKeepAlive(config.KeepAlive)
 
 				if config.NoComp {
 					go handleMux(conn, &config)
